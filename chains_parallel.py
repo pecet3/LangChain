f0e8733e9ba8170a437343705c18da20eb0870ac
Jakub Pacewicz <pecet3107@gmail.com>
2024-07-06 17:56:29 +0200

    .

--- chains_parallel.py
+++ chains_parallel.py
@@ -8,21 +8,57 @@ load_dotenv()
 
 model = ChatOpenAI(model="gpt-3.5-turbo")
 
-messages = [
-    ("system", "You are a christian bot who tells jokes about {topic}. You are responding in polish"),
-    ("human", "Tell me {joke_count} jokes")
-]
 
-prompt_template = ChatPromptTemplate.from_messages(messages)
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+    ("system", "You are an expert product reviewer. You are responding in polish"),
+    ("human", "List the main features of product - {product_name}")
+])
 
-topic = input("Topic: ")
-count = input("Count: ")
 
-uppercase_output = RunnableLambda(lambda x: x.upper())
+def analyze_pros(features):
+    pros_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a product reviewer"),
+            ("human", "Given these features:{features}, list the pros of these features")
+        ]
+    )
 
-count_words = RunnableLambda(lambda x:  f"Word count: {len(x.split())}\n {x}")
+    return pros_template.format_prompt(features=features)
 
-chain = prompt_template | model |StrOutputParser()| uppercase_output | count_words
+def analyze_cons(features):
+    pros_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a product reviewer"),
+            ("human", "Given these features:{features}, list the cons of these features")
+        ]
+        )
 
-result = chain.invoke({"topic": topic,"joke_count":count})
-print(result)
+    return pros_template.format_prompt(features=features)
+
+def combine_pros_cons(pros, cons):
+    return f"Pros:\n{pros}\nCons:\n{cons}"
+
+product = input("Product: ")
+
+cons_branch = (
+    RunnableLambda(lambda x: analyze_cons(x))| model | StrOutputParser()
+)
+pros_branch = (
+    RunnableLambda(lambda x: analyze_pros(x)) | model |StrOutputParser()
+)
+
+chain = (
+    prompt_template
+    | model
+    | StrOutputParser()
+    | RunnableParallel(branches={"pros": pros_branch, "cons":cons_branch})
+    | RunnableLambda(lambda x: combine_pros_cons(x["branches"]["pros"], x["branches"]["cons"]))
+)
+
+try:
+    result = chain.invoke({"product_name": product})
+    print(result)
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
+    print("Traceback:")
