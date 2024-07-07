fa27131fc731d22b4e91e59df3de89eb171902ee
Jakub Pacewicz <pecet3107@gmail.com>
2024-07-07 15:08:43 +0200

    ok

--- rag.py
+++ rag.py
@@ -6,4 +6,36 @@ from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(current_dir, "")
+file_path = os.path.join(current_dir, "books","socket.txt")
+persistent_directory = os.path.join(current_dir,"db","chroma_db")
+
+if not os.path.exists(persistent_directory):
+    print("Initializaing a vector store")
+
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(
+            f"The file {file_path} does not exist..."
+        )
+    
+    loader = TextLoader(file_path)
+    documents = loader.load()
+
+    text_splitter = CharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+    docs = text_splitter.split_documents(documents)
+
+    print(f"Sample Chunk: {docs[0].page_content}")
+
+    embedings= OpenAIEmbeddings(
+        model="text-embeding-3-small"
+    )
+
+    print("Finished embedings")
+
+    db = Chroma.from_documents(
+        docs, embedings, persist_directory=persistent_directory
+    )
+
+    print("Vector store created")
+
+else:
+    print("Vector store exists")
